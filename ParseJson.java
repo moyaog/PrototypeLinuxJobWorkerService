c8fed05c9479148c2b8732821c5769e03b8aeb6b
moya <moya.ogrady@gmail.com>
2019-06-04 15:35:40 -0700

    Fixing parse

--- ParseJson.java
+++ ParseJson.java
@@ -15,7 +15,20 @@ public class ParseJson {
       messageParams.put(RESULT, jsonObj.get(RESULT));
     }
     if(jsonObj.has(PARAMS)) {
-      messageParams.put(PARAMS, jsonObj.get(PARAMS));
+      System.out.println(jsonObj.toString());
+      System.out.println(jsonObj.get(PARAMS).toString());
+      JSONObject param = new JSONObject(jsonObj.get(PARAMS));
+      Request request = new Request();
+      System.out.println(param.toString());
+      /*if(!JSONObject.NULL.equals(param.get(PROCESS))) {
+        //messageParams.put(PROCESS, param.get(PROCESS));
+        request.setProcess(param.get(PROCESS));
+      }
+      if(!JSONObject.NULL.equals(param.get(PID))) {
+        request.setPid(param.get(PID));
+      }
+      //messageParams.put(PARAMS, jsonObj.get(PARAMS));
+      messageParams.put(PARAMS, request);*/
     }
     messageParams.put(ID, jsonObj.get(ID));
 

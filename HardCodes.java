72ab1771fe18a5d2cf47bea1ffb416410c1aff5a
moyaog <moya.ogrady@gmail.com>
2019-06-16 23:55:49 -0700

    Added HardCodes file

---
+++ HardCodes.java
@@ -0,0 +1,16 @@
+package my;
+
+public class HardCodes {
+  private HardCodes() {}
+
+  public static final String HOST = ;
+  public static final int PORT = ;
+  public static final String KEY_STORE = "JKS";
+  public static final String SERVER_KEY_LOC = ;
+  public static final String CLIENT_KEY_LOC = ;
+  public static final String FAKE_KEY_LOC = ;
+  public static final String SERVER_PASSWORD = ;
+  public static final String CLIENT_PASSWORD = ;
+  public static final String TRUST_MANAGER = "PKIX";
+  public static final String PROTOCOL_VER = "TLSv1.2";
+}
